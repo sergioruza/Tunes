8d275601aa898555571020956c40e394a67568b2
Sergio <sergioruza22@gmail.com>
2022-08-30 17:32:11 -0300

    Requisito quatro, ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -25,7 +26,11 @@ export default class Header extends Component {
         {
           loading ? <Loading /> : <h2 data-testid="header-user-name">{ userName }</h2>
         }
-
+        <Link to="/search" data-testid="link-to-search">Pesquisar</Link>
+        <br />
+        <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
+        <br />
+        <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
       </header>
     );
   }
